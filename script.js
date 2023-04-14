cfa26310c02fbaf22364f7b19c41d2636c376df6
Rahul Sharma <rahulsh3105@gmail.com>
2023-04-14 09:41:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+// Create student object with name property
+const student = {
+  name: "Rahul Sharma"
+};
+
+// Add getKeys() method to Object prototype
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+}
+
+// Test getKeys() method on student object
+console.log(student.getKeys()); // Output: ["name"]
